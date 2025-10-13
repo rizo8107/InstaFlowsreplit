81cd67aad94e4ae03083d41c589476a4f7eae5af
Khushboo Bist <khushboobist@gmail.com>
2025-10-13 18:06:50 +0530

    fix: update pg import to use named import from package

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
-import { Pool } from 'pg';
+import pkg from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "@shared/schema";
+const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
